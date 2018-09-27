153a83679fc6704fc78b508bd3615c771959dd97
minavadamee20 <34077203+minavadamee20@users.noreply.github.com>
2018-09-26 20:49:54 -0700

    added vowel labels
    added a bunch of if statements essentially

--- prob2.h
+++ prob2.h
@@ -7,8 +7,39 @@ void prompts() {
 }
 
 void getSentence() {
-	std::cin.get(c);
+	std::cin.get(d);
 }
-void somefunction() {}
+void somefunction() {
+	_asm {
+		call prompts;
+		call getSentence;
+	phaseone:
+		cmp d, '\n';
+		Je done;
+		cmp d, 'a';
+		Je vowelA;
+		cmp d, 'e';
+		Je vowelE;
+		cmp d, 'i';
+		Je vowelI;
+		cmp d, 'o';
+		Je vowelO;
+		cmp d, 'u';
+		Je vowelU;
+	vowelA:
+		  
+
+
+
+	done:
+		
+	}
+
+}
 
 #endif // !prob2_h
+/*what we want to do:
+		maybe we just use a bunch of if else statements and a lot of go to labels. Remember that to make a toupper function we have
+		to subtract 32 from a lowercase to make an uppercase letter
+		to change the lowecase letter to an uppercase letter, we have get the decimal value of the letter and then subtract 32.
+	*/
